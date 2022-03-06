2bf63cf1fcd7380cc2d4a0e0e5f3bbe89905d487
Alex <alex@gu3.me>
2022-03-06 23:44:03 +0000

    Tidy up extensions

--- OpenTPW/Extensions.cs
+++ OpenTPW/Extensions.cs
@@ -1,16 +1,16 @@
 ﻿public static class ByteExtension
 {
-	public static bool GetBit( this byte @byte, int index )
+	public static bool GetBit( this byte self, int index )
 	{
-		var value = ((@byte >> (7 - index)) & 1) != 0;
+		var value = ((self >> (7 - index)) & 1) != 0;
 		return value;
 	}
-	public static bool[] GetBits( this byte @byte, params int[] indices )
+	public static bool[] GetBits( this byte self, params int[] indices )
 	{
 		var bits = new bool[indices.Length];
 		for ( var i = 0; i < indices.Length; ++i )
 		{
-			bits[i] = @byte.GetBit( indices[i] );
+			bits[i] = self.GetBit( indices[i] );
 		}
 		return bits;
 	}
@@ -18,11 +18,12 @@
 
 public static class BoolArrayExtension
 {
-	public static bool ValuesEqual( this bool[] a, bool[] b )
+	public static bool ValuesEqual( this bool[] self, bool[] other )
 	{
-		for ( var i = 0; i < Math.Min( a.Length, b.Length ); ++i )
+		for ( var i = 0; i < Math.Min( self.Length, other.Length ); ++i )
 		{
-			if ( a[i] != b[i] ) return false;
+			if ( self[i] != other[i] )
+				return false;
 		}
 		return true;
 	}
