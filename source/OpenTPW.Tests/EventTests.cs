90505e74289de3a18de0dbc762db8e0f1b8e78f1
Alex <alex@gu3.me>
2022-03-18 22:22:41 +0000

    Initial attribute-based event system

---
+++ source/OpenTPW.Tests/EventTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OpenTPW.Tests;
+
+[TestClass]
+public class EventTests
+{
+	partial class TestEvent
+	{
+		public class Test
+		{
+			public class TestEventAttribute : EventAttribute
+			{
+				public const string Name = "Event.Test.TestEvent";
+				public TestEventAttribute() : base( Name ) { }
+			}
+		}
+	}
+
+	static bool eventHasRun = false;
+
+	[TestMethod]
+	public void RegisterAndRunStaticEvent()
+	{
+		Event.Register( this );
+		Event.Run( "Event.Test.TestEvent" );
+
+		Assert.IsTrue( eventHasRun );
+	}
+
+	[TestEvent.Test.TestEvent]
+	public static void StaticEvent()
+	{
+		eventHasRun = true;
+	}
+}
---
+++ source/OpenTPW/Events/Event.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Reflection;
+
+namespace OpenTPW;
+
+public static partial class Event
+{
+	struct EventRef
+	{
+		public EventRef( string name, MethodInfo method )
+		{
+			this.name = name;
+			this.method = method;
+		}
+
+		public string name { get; set; }
+		public MethodInfo method { get; set; }
+
+
+	}
+	private static List<EventRef> events = new();
+
+	public static void Register( object obj )
+	{
+		var attributes = obj.GetType().GetMethods()
+			.Where( m => m.GetCustomAttribute<EventAttribute>() != null )
+			.Select( m => new EventRef( m.GetCustomAttribute<EventAttribute>().EventName, m ) );
+
+		events.AddRange( attributes );
+	}
+
+	public static void Run( string name )
+	{
+		events.ForEach( e =>
+		{
+			if ( e.name == name )
+				e.method?.Invoke( null, null );
+		} );
+	}
+}
+
+[AttributeUsage( AttributeTargets.Method, AllowMultiple = true )]
+public class EventAttribute : Attribute
+{
+	public string EventName { get; set; }
+
+	public EventAttribute( string eventName )
+	{
+		EventName = eventName;
+	}
+}
+
+partial class Event
+{
+	public class Game
+	{
+		public class LoadAttribute : EventAttribute
+		{
+			const string EventName = "Event.Game.Load";
+			public LoadAttribute() : base( EventName ) { }
+		}
+	}
+}
