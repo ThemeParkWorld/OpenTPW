94c4888082fb5061d1ba86e63915e17cce1c20f7
Alex <alex@gu3.me>
2022-03-19 23:34:29 +0000

    Label jump to & opcode info tooltip

--- source/OpenTPW/Editor/Tabs/RidesTab.cs
+++ source/OpenTPW/Editor/Tabs/RidesTab.cs
@@ -1,5 +1,6 @@
 ﻿using ImGuiNET;
 using System.Numerics;
+using System.Reflection;
 
 namespace OpenTPW;
 internal class RidesTab : BaseTab
@@ -9,7 +10,7 @@ internal class RidesTab : BaseTab
 	struct Colors
 	{
 		public System.Numerics.Vector4 Background => MathExtensions.GetColor( "#282C34" );
-		public System.Numerics.Vector4 Error => MathExtensions.GetColor( "#E06C75" );
+		public System.Numerics.Vector4 Variable => MathExtensions.GetColor( "#E06C75" );
 		public System.Numerics.Vector4 String => MathExtensions.GetColor( "#98C379" );
 		public System.Numerics.Vector4 Literal => MathExtensions.GetColor( "#E5C07B" );
 		public System.Numerics.Vector4 Label => MathExtensions.GetColor( "#61AFEF" );
@@ -32,6 +33,8 @@ internal class RidesTab : BaseTab
 
 		ImGui.PopStyleColor();
 	}
+
+	private Dictionary<int, float> labelPositions = new(); // TODO: Refresh this on script change
 
 	public override void Draw()
 	{
@@ -66,7 +69,6 @@ internal class RidesTab : BaseTab
 
 		ImGui.Separator();
 
-
 		//
 		// Disassembly view
 		//
@@ -86,13 +88,35 @@ internal class RidesTab : BaseTab
 				if ( vm.Branches.Any( b => b.CompiledOffset == labelOffset ) )
 				{
 					ImGui.NewLine();
+
 					DrawColoredText( $".label_{labelOffset}", colors.Label );
+
+					if ( !labelPositions.ContainsKey( labelOffset ) )
+						labelPositions.Add( labelOffset, ImGui.GetCursorPosY() );
 				}
 
 				ImGui.SetCursorPos( ImGui.GetCursorPos() + padding );
-				DrawColoredText( $"0x{instruction.offset:X4}: ", colors.Comment );
+				DrawColoredText( $"0x{instruction.offset:X4}: ", colors.Generic );
 				ImGui.SameLine();
+
 				DrawColoredText( $"{instruction.opcode}", colors.Instruction );
+
+				// Opcode info tooltip
+				if ( ImGui.IsItemHovered() )
+				{
+					ImGui.BeginTooltip();
+					ImGui.Text( $"{instruction.opcode}" );
+
+					var opcodeHandler = vm.FindOpcodeHandler( instruction.opcode )?.GetCustomAttribute<OpcodeHandlerAttribute>();
+
+					if ( opcodeHandler != null )
+					{
+						ImGui.PushFont( Editor.SansSerifFont );
+						ImGui.Text( $"{opcodeHandler.Description}" );
+						ImGui.PopFont();
+					}
+					ImGui.End();
+				}
 
 				//
 				// Draw operands
@@ -103,7 +127,7 @@ internal class RidesTab : BaseTab
 
 					var color = operand.type switch
 					{
-						Operand.Type.Variable => colors.Generic,
+						Operand.Type.Variable => colors.Variable,
 						Operand.Type.Literal => colors.Literal,
 						Operand.Type.String => colors.String,
 						Operand.Type.Location => colors.Label,
@@ -111,6 +135,34 @@ internal class RidesTab : BaseTab
 					};
 
 					DrawColoredText( $"{operand}", color );
+
+					// Link hover, jump to on click
+					if ( ImGui.IsItemHovered() && operand.type == Operand.Type.Location )
+					{
+						var itemP1 = ImGui.GetItemRectMin();
+						var itemP2 = ImGui.GetItemRectMax();
+
+						itemP1.Y = itemP2.Y;
+						itemP2.X = itemP1.X + ImGui.CalcTextSize( operand.ToString() ).X;
+
+						var drawList = ImGui.GetWindowDrawList();
+						var underlineCol = ImGui.GetColorU32( color );
+						drawList.AddLine( itemP1, itemP2, underlineCol );
+
+						if ( ImGui.IsItemClicked() )
+						{
+							// Jump to
+							if ( labelPositions.TryGetValue( operand.Value, out var position ) )
+							{
+								Log.Trace( "Jumped to " + operand.Value );
+								ImGui.SetScrollY( position - 32f );
+							}
+							else
+							{
+								Log.Trace( "Couldn't jump to " + operand.Value );
+							}
+						}
+					}
 				}
 
 				labelOffset += instruction.GetCount();
