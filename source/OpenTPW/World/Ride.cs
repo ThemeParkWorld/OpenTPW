88e9fcd47496e637b38baa0973048ded0cb0fb26
Alex <alex@gu3.me>
2022-03-19 17:04:42 +0000

    Watch for script changes, refresh VM

--- source/OpenTPW/World/Ride.cs
+++ source/OpenTPW/World/Ride.cs
@@ -3,6 +3,7 @@
 public class Ride : Entity
 {
 	private WadFileSystem fileSystem;
+	private FileSystemWatcher watcher;
 
 	public RideVM VM { get; private set; }
 
@@ -12,9 +13,48 @@ public class Ride : Entity
 		var rideName = Path.GetFileNameWithoutExtension( rideArchive );
 
 		Log.Trace( $"Loading ride {rideName}" );
-		// VM = new RideVM( fileSystem.OpenRead( rideName + ".rse" ) );
 
-		using var testScriptStream = File.OpenRead( "content/testscripts/test.rse" );
+		watcher = new FileSystemWatcher( "content/testscripts", "test.rse" );
+
+		watcher.NotifyFilter = NotifyFilters.Attributes
+							 | NotifyFilters.CreationTime
+							 | NotifyFilters.DirectoryName
+							 | NotifyFilters.FileName
+							 | NotifyFilters.LastAccess
+							 | NotifyFilters.LastWrite
+							 | NotifyFilters.Size;
+		watcher.EnableRaisingEvents = true;
+		watcher.Changed += Watcher_Changed;
+
+		CreateVM();
+
+		// VM = new RideVM( fileSystem.OpenRead( rideName + ".rse" ) );
+	}
+
+	private void Watcher_Changed( object sender, FileSystemEventArgs e )
+	{
+		Log.Trace( $"File {e.Name} changed" );
+		CreateVM();
+	}
+	public static bool IsFileReady( string path )
+	{
+		try
+		{
+			using ( FileStream inputStream = File.Open( path, FileMode.Open, FileAccess.Read, FileShare.None ) )
+				return inputStream.Length > 0;
+		}
+		catch ( Exception )
+		{
+			return false;
+		}
+	}
+
+	private void CreateVM()
+	{
+		var path = "content/testscripts/test.rse";
+		while ( !IsFileReady( path ) ) ;
+
+		using var testScriptStream = File.OpenRead( path );
 		VM = new RideVM( testScriptStream );
 	}
 
