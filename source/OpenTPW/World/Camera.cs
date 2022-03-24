f4605e0e96a0564317b43fc04ad0e825ff520c39
Alex <alex@gu3.me>
2022-03-24 13:10:33 +0000

    World: Improve camera slightly

--- source/OpenTPW/World/Camera.cs
+++ source/OpenTPW/World/Camera.cs
@@ -9,11 +9,11 @@ public class Camera : Entity
 
 	private void CalcViewProjMatrix()
 	{
-		var cameraPos = new Vector3( position.X, position.Y, 4 );
-		var cameraFront = new Vector3( 0, 0, -1 );
+		var cameraPos = new Vector3( position.X, position.Y, position.Z );
+		var lookAt = new Vector3(position.X, position.Y + 4, position.Z - 8 ); // new Vector3( position.X, position.Y + 16, position.Z + 16 );
 		var cameraUp = new Vector3( 0, 1, 0 );
 
-		ViewMatrix = Silk.NET.Maths.Matrix4X4.CreateLookAt<float>( cameraPos, cameraPos + cameraFront, cameraUp );
+		ViewMatrix = Silk.NET.Maths.Matrix4X4.CreateLookAt<float>( cameraPos, lookAt, cameraUp );
 		ProjMatrix = Silk.NET.Maths.Matrix4X4.CreatePerspectiveFieldOfView<float>(
 			90.0f.DegreesToRadians(),
 			Screen.Aspect,
@@ -22,12 +22,15 @@ public class Camera : Entity
 		);
 	}
 
+	private float cameraSpeed => 4f;
+
 	public override void Update()
 	{
 		base.Update();
 
-		position += Vector3.UnitX * Input.Right * Time.Delta;
-		position += Vector3.UnitY * Input.Forward * Time.Delta;
+		position += Vector3.UnitX * Input.Right * Time.Delta * cameraSpeed;
+		position += Vector3.UnitY * Input.Forward * Time.Delta * cameraSpeed;
+		position.Z = 8;
 
 		CalcViewProjMatrix();
 	}
