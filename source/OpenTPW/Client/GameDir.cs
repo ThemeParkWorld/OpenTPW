1b760bbf5b50399444245e6159bec7d768ad408f
Alex <alex@gu3.me>
2022-03-24 12:59:22 +0000

    Misc: formatting fixes, editorconfig tweaks

--- source/OpenTPW/Client/GameDir.cs
+++ source/OpenTPW/Client/GameDir.cs
@@ -5,13 +5,13 @@
 /// </summary>
 public static class GameDir
 {
-    /// <summary>
-    /// Joins <see cref="Settings.Default.GamePath"/> to <param name="path">path</param>.
-    /// </summary>
-    /// <param name="path"></param>
-    /// <returns></returns>
-    public static string GetPath( string path )
-    {
-        return Path.Join( Settings.Default.GamePath, path );
-    }
+	/// <summary>
+	/// Joins <see cref="Settings.Default.GamePath"/> to <param name="path">path</param>.
+	/// </summary>
+	/// <param name="path"></param>
+	/// <returns></returns>
+	public static string GetPath( string path )
+	{
+		return Path.Join( Settings.Default.GamePath, path );
+	}
 }
--- source/OpenTPW/Editor/Editor.cs
+++ source/OpenTPW/Editor/Editor.cs
@@ -7,132 +7,133 @@ using System.Runtime.InteropServices;
 namespace OpenTPW;
 internal partial class Editor
 {
-    private ImGuiController ImGuiController;
-    private Texture defaultFontTexture;
+	private ImGuiController ImGuiController;
+	private Texture defaultFontTexture;
 
-    private List<BaseTab> tabs = new();
+	private List<BaseTab> tabs = new();
 
-    private bool shouldRender;
+	private bool shouldRender;
 
-    public static ImFontPtr MonospaceFont { get; private set; }
-    public static ImFontPtr SansSerifFont { get; private set; }
+	public static ImFontPtr MonospaceFont { get; private set; }
+	public static ImFontPtr SansSerifFont { get; private set; }
 
-    public Editor( ImGuiController imGuiController )
-    {
-        ImGuiController = imGuiController;
+	public Editor( ImGuiController imGuiController )
+	{
+		ImGuiController = imGuiController;
 
-        InitIO();
-        SetTheme();
+		InitIO();
+		SetTheme();
 
-        tabs.AddRange( new BaseTab[] {
-            new TexturesTab(),
-            new ConsoleTab(),
-            new InputTab(),
-            new RidesTab(),
-            new FileBrowserTab(),
-            new DemoWindow(),
-            new CursorTab()
-        } );
-    }
+		tabs.AddRange( new BaseTab[] {
+			new TexturesTab(),
+			new ConsoleTab(),
+			new InputTab(),
+			new RidesTab(),
+			new FileBrowserTab(),
+			new DemoWindow(),
+			new CursorTab(),
+			new SceneTab()
+		} );
+	}
 
-    private static void SetKeyMappings( ImGuiIOPtr io )
-    {
-        io.KeyMap[(int)ImGuiKey.Tab] = (int)Key.Tab;
-        io.KeyMap[(int)ImGuiKey.LeftArrow] = (int)Key.Left;
-        io.KeyMap[(int)ImGuiKey.RightArrow] = (int)Key.Right;
-        io.KeyMap[(int)ImGuiKey.UpArrow] = (int)Key.Up;
-        io.KeyMap[(int)ImGuiKey.DownArrow] = (int)Key.Down;
-        io.KeyMap[(int)ImGuiKey.PageUp] = (int)Key.PageUp;
-        io.KeyMap[(int)ImGuiKey.PageDown] = (int)Key.PageDown;
-        io.KeyMap[(int)ImGuiKey.Home] = (int)Key.Home;
-        io.KeyMap[(int)ImGuiKey.End] = (int)Key.End;
-        io.KeyMap[(int)ImGuiKey.Delete] = (int)Key.Delete;
-        io.KeyMap[(int)ImGuiKey.Backspace] = (int)Key.Backspace;
-        io.KeyMap[(int)ImGuiKey.Enter] = (int)Key.Enter;
-        io.KeyMap[(int)ImGuiKey.Escape] = (int)Key.Escape;
-        io.KeyMap[(int)ImGuiKey.A] = (int)Key.A;
-        io.KeyMap[(int)ImGuiKey.C] = (int)Key.C;
-        io.KeyMap[(int)ImGuiKey.V] = (int)Key.V;
-        io.KeyMap[(int)ImGuiKey.X] = (int)Key.X;
-        io.KeyMap[(int)ImGuiKey.Y] = (int)Key.Y;
-        io.KeyMap[(int)ImGuiKey.Z] = (int)Key.Z;
-    }
+	private static void SetKeyMappings( ImGuiIOPtr io )
+	{
+		io.KeyMap[(int)ImGuiKey.Tab] = (int)Key.Tab;
+		io.KeyMap[(int)ImGuiKey.LeftArrow] = (int)Key.Left;
+		io.KeyMap[(int)ImGuiKey.RightArrow] = (int)Key.Right;
+		io.KeyMap[(int)ImGuiKey.UpArrow] = (int)Key.Up;
+		io.KeyMap[(int)ImGuiKey.DownArrow] = (int)Key.Down;
+		io.KeyMap[(int)ImGuiKey.PageUp] = (int)Key.PageUp;
+		io.KeyMap[(int)ImGuiKey.PageDown] = (int)Key.PageDown;
+		io.KeyMap[(int)ImGuiKey.Home] = (int)Key.Home;
+		io.KeyMap[(int)ImGuiKey.End] = (int)Key.End;
+		io.KeyMap[(int)ImGuiKey.Delete] = (int)Key.Delete;
+		io.KeyMap[(int)ImGuiKey.Backspace] = (int)Key.Backspace;
+		io.KeyMap[(int)ImGuiKey.Enter] = (int)Key.Enter;
+		io.KeyMap[(int)ImGuiKey.Escape] = (int)Key.Escape;
+		io.KeyMap[(int)ImGuiKey.A] = (int)Key.A;
+		io.KeyMap[(int)ImGuiKey.C] = (int)Key.C;
+		io.KeyMap[(int)ImGuiKey.V] = (int)Key.V;
+		io.KeyMap[(int)ImGuiKey.X] = (int)Key.X;
+		io.KeyMap[(int)ImGuiKey.Y] = (int)Key.Y;
+		io.KeyMap[(int)ImGuiKey.Z] = (int)Key.Z;
+	}
 
-    private void InitIO()
-    {
-        var io = ImGui.GetIO();
+	private void InitIO()
+	{
+		var io = ImGui.GetIO();
 
-        io.ConfigFlags |= ImGuiConfigFlags.DockingEnable | ImGuiConfigFlags.IsSRGB;
+		io.ConfigFlags |= ImGuiConfigFlags.DockingEnable | ImGuiConfigFlags.IsSRGB;
 
-        io.Fonts.Clear();
+		io.Fonts.Clear();
 
-        SansSerifFont = io.Fonts.AddFontFromFileTTF( "content/fonts/Roboto-Regular.ttf", 14f );
-        MonospaceFont = io.Fonts.AddFontDefault();
+		SansSerifFont = io.Fonts.AddFontFromFileTTF( "content/fonts/Roboto-Regular.ttf", 14f );
+		MonospaceFont = io.Fonts.AddFontDefault();
 
-        io.Fonts.GetTexDataAsRGBA32( out IntPtr pixels, out var width, out var height, out var bpp );
+		io.Fonts.GetTexDataAsRGBA32( out IntPtr pixels, out var width, out var height, out var bpp );
 
-        int size = width * height * bpp;
-        byte[] data = new byte[size];
-        Marshal.Copy( pixels, data, 0, size );
-        defaultFontTexture = TextureBuilder.FromBytes( data, (uint)width, (uint)height ).Build();
+		int size = width * height * bpp;
+		byte[] data = new byte[size];
+		Marshal.Copy( pixels, data, 0, size );
+		defaultFontTexture = TextureBuilder.FromBytes( data, (uint)width, (uint)height ).Build();
 
-        io.Fonts.SetTexID( (IntPtr)defaultFontTexture.Id );
-        io.Fonts.ClearTexData();
+		io.Fonts.SetTexID( (IntPtr)defaultFontTexture.Id );
+		io.Fonts.ClearTexData();
 
-        SetKeyMappings( io );
-    }
+		SetKeyMappings( io );
+	}
 
-    private void DrawMenuBar()
-    {
-        ImGui.BeginMainMenuBar();
+	private void DrawMenuBar()
+	{
+		ImGui.BeginMainMenuBar();
 
-        ImGui.Text( "OpenTPW |" );
+		ImGui.Text( "OpenTPW |" );
 
-        foreach ( var tab in tabs )
-        {
-            var editorMenuAttribute = tab.GetType().GetCustomAttribute<EditorMenuAttribute>();
-            if ( editorMenuAttribute == null )
-                continue;
+		foreach ( var tab in tabs )
+		{
+			var editorMenuAttribute = tab.GetType().GetCustomAttribute<EditorMenuAttribute>();
+			if ( editorMenuAttribute == null )
+				continue;
 
-            var splitPath = editorMenuAttribute.Path.Split( '/' );
+			var splitPath = editorMenuAttribute.Path.Split( '/' );
 
-            if ( ImGui.BeginMenu( splitPath[0] ) )
-            {
-                for ( int i = 1; i < splitPath.Length; i++ )
-                {
-                    string? item = splitPath[i];
-                    bool active = ImGui.MenuItem( item );
+			if ( ImGui.BeginMenu( splitPath[0] ) )
+			{
+				for ( int i = 1; i < splitPath.Length; i++ )
+				{
+					string? item = splitPath[i];
+					bool active = ImGui.MenuItem( item );
 
-                    if ( i == splitPath.Length - 1 && active )
-                        tab.visible = !tab.visible;
-                }
+					if ( i == splitPath.Length - 1 && active )
+						tab.visible = !tab.visible;
+				}
 
-                ImGui.EndMenu();
-            }
-        }
+				ImGui.EndMenu();
+			}
+		}
 
-        ImGui.EndMainMenuBar();
-    }
+		ImGui.EndMainMenuBar();
+	}
 
-    public void Update()
-    {
-        ImGuiController.Update( Time.Delta );
+	public void Update()
+	{
+		ImGuiController.Update( Time.Delta );
 
-        if ( Input.Pressed( InputButton.ConsoleToggle ) )
-            shouldRender = !shouldRender;
+		if ( Input.Pressed( InputButton.ConsoleToggle ) )
+			shouldRender = !shouldRender;
 
-        if ( !shouldRender )
-            return;
+		if ( !shouldRender )
+			return;
 
-        DrawMenuBar();
+		DrawMenuBar();
 
-        ImGui.DockSpaceOverViewport( ImGui.GetMainViewport(), ImGuiDockNodeFlags.PassthruCentralNode );
+		ImGui.DockSpaceOverViewport( ImGui.GetMainViewport(), ImGuiDockNodeFlags.PassthruCentralNode );
 
-        tabs.ForEach( tab =>
-        {
-            if ( tab.visible )
-                tab.Draw();
-        } );
-    }
+		tabs.ForEach( tab =>
+		{
+			if ( tab.visible )
+				tab.Draw();
+		} );
+	}
 }
 
