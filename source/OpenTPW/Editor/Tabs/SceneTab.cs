7dad2bd6d819a896a7f891b16bd66cf11ab72ec2
Alex <alex@gu3.me>
2022-03-24 13:14:08 +0000

    Editor: add scene outliner

---
+++ source/OpenTPW/Editor/Tabs/SceneTab.cs
@@ -0,0 +1,26 @@
+﻿using ImGuiNET;
+
+namespace OpenTPW;
+
+[EditorMenu( "Scene/Outliner" )]
+internal class SceneTab : BaseTab
+{
+	public override void Draw()
+	{
+		ImGui.Begin( "Scene", ref visible );
+
+		foreach ( var obj in Entity.All )
+		{
+			if ( ImGui.TreeNode( obj.Name ) )
+			{
+				ImGui.Text( $"P: {obj.position}" );
+				ImGui.Text( $"R: {obj.rotation}" );
+				ImGui.Text( $"S: {obj.scale}" );
+
+				ImGui.TreePop();
+			}
+		}
+
+		ImGui.End();
+	}
+}
