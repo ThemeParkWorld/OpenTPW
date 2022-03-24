15c3a69c54079c3d0433f46cc285a683dddf574b
Alex <alex@gu3.me>
2022-03-24 12:58:47 +0000

    Misc: fix screen aspect ratio calculation

--- source/OpenTPW/Global/Screen.cs
+++ source/OpenTPW/Global/Screen.cs
@@ -4,7 +4,7 @@ public static class Screen
 {
 	public static Point2 Size { get; set; } = new( 1, 1 );
 
-	public static float Aspect => (float)Size.Y / (float)Size.X;
+	public static float Aspect => (float)Size.X / (float)Size.Y;
 
 	public static void UpdateFrom( Point2 size )
 	{
