43df2b6716ccb59e2d256b547e88bc5b4b94b040
Alex <alex@gu3.me>
2022-03-27 02:00:15 +0100

    Misc: add bbox structure

---
+++ source/OpenTPW/Utils/BBox.cs
@@ -0,0 +1,19 @@
+﻿namespace OpenTPW;
+
+public struct BBox
+{
+	public Vector3 Mins { get; set; }
+	public Vector3 Maxs { get; set; }
+
+	public BBox()
+	{
+		Mins = default;
+		Maxs = default;
+	}
+
+	public BBox( Vector3 mins, Vector3 maxs )
+	{
+		Mins = mins;
+		Maxs = maxs;
+	}
+}
