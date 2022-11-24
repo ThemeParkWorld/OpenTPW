ba7949157817a473b5898521c879e54bb4e17cf1
Alex <alex@gu3.me>
2022-11-24 05:38:24 +0000

    ImGUI wct viewer (could probs consolidate into base class)

---
+++ source/OpenTPW/Editor/Tabs/FileBrowser/Handlers/TextureFileHandler.cs
@@ -0,0 +1,36 @@
+﻿using ImGuiNET;
+using System.Numerics;
+
+namespace OpenTPW;
+
+[FileHandler( @"\.wct", icon: "content/icons/image.png" )]
+public class TextureFileHandler : BaseFileHandler
+{
+	private Texture texture;
+
+	public TextureFileHandler( byte[] fileData ) : base( fileData )
+	{
+		using var fileStream = new MemoryStream( fileData );
+		texture = new TextureFile( fileStream ).Texture;
+	}
+
+	public override void Draw()
+	{
+		base.Draw();
+
+		ImGui.Text( $"File type: Texture" );
+
+		var windowWidth = ImGui.GetWindowSize().X;
+		float ratio = texture.Height / (float)texture.Width;
+
+		if ( ratio is float.NaN )
+			ratio = 1f;
+
+		EditorHelpers.Image( texture, new Vector2( windowWidth, windowWidth * ratio ) );
+
+		var startPos = ImGui.GetItemRectMin();
+
+		var drawList = ImGui.GetWindowDrawList();
+		var col = ImGui.GetColorU32( Vector4.One );
+	}
+}
