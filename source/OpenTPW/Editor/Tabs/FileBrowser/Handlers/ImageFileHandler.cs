0463e476c27fb897799a2687b2432e4721d63b05
Alex Guthrie <12881812+xezno@users.noreply.github.com>
2023-06-11 17:16:46 +0100

    Remove ugly grid in image file handler

--- source/OpenTPW/Editor/Tabs/FileBrowser/Handlers/ImageFileHandler.cs
+++ source/OpenTPW/Editor/Tabs/FileBrowser/Handlers/ImageFileHandler.cs
@@ -27,29 +27,5 @@ public class ImageFileHandler : BaseFileHandler
 			ratio = 1f;
 
 		EditorHelpers.Image( texture, new Vector2( windowWidth, windowWidth * ratio ) );
-
-		var startPos = ImGui.GetItemRectMin();
-
-		var drawList = ImGui.GetWindowDrawList();
-		var col = ImGui.GetColorU32( Vector4.One );
-
-		if ( texture.Width > 128 || texture.Height > 128 )
-			return;
-
-		for ( int y = 1; y < texture.Height; y++ )
-		{
-			float lineX = 0f + startPos.X;
-			float lineY = (((windowWidth * ratio) / (float)texture.Height) * y) + startPos.Y;
-
-			drawList.AddLine( new System.Numerics.Vector2( lineX, lineY ), new System.Numerics.Vector2( startPos.X + windowWidth, lineY + 2 ), col );
-		}
-
-		for ( int x = 1; x < texture.Width; x++ )
-		{
-			float lineX = ((windowWidth * ratio / (float)texture.Width) * x) + startPos.X;
-			float lineY = 0f + startPos.Y;
-
-			drawList.AddLine( new System.Numerics.Vector2( lineX, lineY ), new System.Numerics.Vector2( lineX + 2, startPos.Y + (windowWidth * ratio) ), col );
-		}
 	}
 }
