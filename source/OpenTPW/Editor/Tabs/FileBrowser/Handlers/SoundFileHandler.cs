9473f3d52a9493e5e625935cc1ded1e085fafe87
Ben Heapy <ben.heapy@payloadstudios.com>
2023-11-10 09:09:01 +0000

    Handle sound file (first pass) - doesn't work yet but need to figure this out

---
+++ source/OpenTPW/Editor/Tabs/FileBrowser/Handlers/SoundFileHandler.cs
@@ -0,0 +1,52 @@
+﻿using ImGuiNET;
+using OpenTPW.Formats.Sound;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using Vortice.Multimedia;
+
+namespace OpenTPW;
+
+[FileHandler( @"\.mp2" )]
+public class SoundFileHandler : BaseFileHandler
+{
+	private MP2File mp2File;
+	private SoundPlayer sound;
+
+	public SoundFileHandler( byte[] fileData ) : base( fileData )
+	{
+		mp2File = new MP2File( fileData );
+		sound = new SoundPlayer( mp2File.GetStream() );
+	}
+
+	public override void Draw()
+	{
+
+		base.Draw();
+
+		ImGui.Text( $"File type: MP2" );
+		ImGui.PushStyleColor( ImGuiCol.FrameBg, OneDark.Background );
+		ImGui.PushStyleColor( ImGuiCol.Text, OneDark.Generic );
+		ImGui.PushFont( Editor.MonospaceFont );
+
+		sound.Load();
+		if ( ImGui.Button( "Play" ) )
+		{
+			if( sound.IsLoadCompleted ) 
+			{ 
+				sound.Play();
+			}
+		}
+
+		if( ImGui.Button( "Stop" ) )
+		{
+			sound.Stop();
+		}
+
+		ImGui.PopFont();
+		ImGui.PopStyleColor( 2 );
+	}
+}
