e45b5836b721ff0c83a5023b0bee7cc4abc93f8f
Ben Heapy <cronicstate@gmail.com>
2023-11-22 16:36:43 +0000

    SaveFileHandler to view content of Save File string using readers byte to string function

---
+++ source/OpenTPW/Editor/Tabs/FileBrowser/Handlers/SaveFileHandler.cs
@@ -0,0 +1,40 @@
+﻿using ImGuiNET;
+using System.Text;
+using System.Xml;
+
+namespace OpenTPW;
+[FileHandler( @"\.TPWS" )]
+public class SaveFileHandler : BaseFileHandler
+{
+	private SaveFileReader reader;
+	private char[] output;
+	private string text;
+
+	public SaveFileHandler( byte[] fileData ) : base( fileData )
+	{
+		using var stream = new SaveFileStream( fileData );
+		reader = new SaveFileReader( stream );
+		text = reader.FileToString();
+	}
+
+	public override void Draw()
+	{
+		base.Draw();
+
+		ImGui.Text( $"Save File Contents:" );
+		ImGui.PushStyleColor( ImGuiCol.ChildBg, OneDark.Background );
+		ImGui.PushStyleColor( ImGuiCol.Text, OneDark.Generic );
+		ImGui.PushFont( Editor.MonospaceFont );
+
+		ImGui.InputTextMultiline(
+			"##save_file_contents",
+			ref text,
+			(uint)text.Length,
+			new System.Numerics.Vector2( -1, -1 ),
+			ImGuiInputTextFlags.ReadOnly );
+
+		ImGui.PopFont();
+		ImGui.PopStyleColor( 2 );
+	}
+
+}
