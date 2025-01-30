8f7597568ab7d22c964ffb9746857c3dbe2780af
Alex <12881812+xezno@users.noreply.github.com>
2025-01-30 03:15:32 +0000

    Default game path

--- source/OpenTPW/Settings.Designer.cs
+++ source/OpenTPW/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace OpenTPW {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,7 +25,7 @@ namespace OpenTPW {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("G:\\")]
+        [global::System.Configuration.DefaultSettingValueAttribute("C:\\Program Files (x86)\\Bullfrog\\Theme Park World")]
         public string GamePath {
             get {
                 return ((string)(this["GamePath"]));
